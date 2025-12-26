d32b502933e6c58ea397a9d55662ffea82f4ae17
allan jaison dsa <allan.jaison@btech.christuniversity.in>
2025-12-26 19:18:18 +0530

    fixed color issues

--- components/layout/appshell.tsx
+++ components/layout/appshell.tsx
@@ -1,6 +1,6 @@
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen flex bg-black text-neutral-100">
       <aside className="w-60 border-r p-4">Kasparro</aside>
       <main className="flex-1 p-8">{children}</main>
     </div>
